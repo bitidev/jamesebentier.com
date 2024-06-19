d709d4049eb1657c84fe96bc97a5dbffd79edac0
James Ebentier <jebentier@gmail.com>
2024-06-19 21:24:42 +0200

    Set up testing framework

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "3.3.1"
+ruby "~> 3.3"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.4"
@@ -47,9 +47,35 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem "faraday"
+gem "faraday-multipart"
+gem "faraday-retry"
+
+gem "warning"
+
 group :development, :test do
-  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri windows ]
+  gem "danger",              require: false
+  gem "danger-rubocop",      require: false
+
+  gem "pry",                 require: false
+
+  gem "rubocop",             require: false
+  gem "rubocop-performance", require: false
+  gem "rubocop-rails",       require: false
+  gem "rubocop-rspec",       require: false
+end
+
+group :test do
+  gem "database_cleaner-active_record"
+  gem "factory_bot-awesome_linter"
+  gem "factory_bot_rails"
+  gem "faker"
+  gem "rspec"
+  gem "rspec-rails"
+  gem "shoulda-matchers"
+  gem "simplecov", require: false
+  gem "timecop"
+  gem "webmock"
 end
 
 group :development do
@@ -62,4 +88,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 require_relative "boot"
+
+# There are warnings coming out of the mail gem that are not useful, and
+# will be fixed in the next version releases, so we can ignore them.
+require "warning"
+Warning.ignore(/.*statement not reached.*/, /.*mail-2.8.1.*/)
+Warning.ignore(/.*assigned but unused variable.*/, /.*mail-2.8.1.*/)
 
 require "rails"
 # Pick the frameworks you want:
@@ -19,14 +27,14 @@ require "action_cable/engine"
 Bundler.require(*Rails.groups)
 
 module JamesEbentier
-  class Application < Rails::Application
+  class Application < Rails::Application # rubocop:disable Style/Documentation
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -38,5 +46,14 @@ module JamesEbentier
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Don't generate system test files.
+    config.generators do |g|
+      g.test_framework   :rspec, fixtures: true, views: false
+      g.integration_tool :rspec, fixtures: true, views: true
+
+      # Don't generate system test files.
+      g.system_tests = nil
+    end
   end
 end
